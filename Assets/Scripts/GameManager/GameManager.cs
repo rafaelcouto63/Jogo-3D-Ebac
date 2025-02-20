39beee5bbd1b2fd713755dc6d0eb04ed039e8073
rafaelcouto63 <rafacouto63@gmail.com>
2025-02-20 17:51:41 -0300

    Update Game Manager

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -27,7 +27,7 @@ public class GameManager : Singleton<GameManager>
     {
         stateMachine = new StateMachine<GameStates>();
         stateMachine.Init();
-        stateMachine.RegisterStates(GameStates.INTRO, new StateBase());
+        stateMachine.RegisterStates(GameStates.INTRO, new GMStateIntro());
         stateMachine.RegisterStates(GameStates.GAMEPLAY, new StateBase());
         stateMachine.RegisterStates(GameStates.PAUSE, new StateBase());
         stateMachine.RegisterStates(GameStates.WIN, new StateBase());

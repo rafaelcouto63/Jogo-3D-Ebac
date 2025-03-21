5cfabb08846a37ce6214f50f53e8c710a6a11ce0
rafaelcouto63 <rafacouto63@gmail.com>
2025-03-21 16:16:01 -0300

    Add ItemManager

--- Assets/Scripts/Item/CollectableBaseCoin.cs
+++ Assets/Scripts/Item/CollectableBaseCoin.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Itens;
 
 public class CollectableBaseCoin : CollectableBase
 {
-    public Collider2D collider;
+    public Collider collider;
     public int amount = 1;
     protected override void OnCollect()
     {
         base.OnCollect();
-        ItemManager.instance.AddCoin(amount);
+        ItemManager.Instance.AddByType(ItemType.COIN);
         collider.enabled = false;
     }
 }
--- Assets/Scripts/Item/ItemManager.cs
+++ Assets/Scripts/Item/ItemManager.cs
@@ -1,25 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using TMPro;
+using Ebac.Singleton;
 
-public class ItemManager : MonoBehaviour
+namespace Itens
 {
-    public static ItemManager instance;
-    public SOint coins;
-    public TextMeshProUGUI coinText;
-
-    private void Awake() 
-    {
-        if(instance == null) 
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+ public enum ItemType
+ {
+    COIN,
+    LIFE_PACK
+ }
+ public class ItemManager : Singleton<ItemManager>
+ {
+    public List<ItenSetup> itenSetups;
 
     private void Start()
     {
@@ -28,18 +23,50 @@ public class ItemManager : MonoBehaviour
 
     private void Reset()
     {
-        coins.value = 0;
-        //UpdateCoinText();
+        foreach(var i in itenSetups) 
+        {
+           i.soInt.value = 0; 
+        }
+       
     }
 
-    public void AddCoin(int amount)
+    public void AddByType(ItemType itemType, int amount = 1)
     {
-        coins.value += amount;
-        //UpdateCoinText();
+        if(amount < 0) return;
+
+        itenSetups.Find(i => i.itemType == itemType).soInt.value += amount;
     }
 
-    private void UpdateCoinText()
+    public void RemoveByType(ItemType itemType, int amount = 1)
     {
-        coinText.text = "X " + coins.value.ToString(); 
+        if(amount > 0) return;
+        
+        var iten = itenSetups.Find(i => i.itemType == itemType);
+        iten.soInt.value -= amount;
+
+        if(iten.soInt.value < 0) 
+        {
+            iten.soInt.value = 0;
+        }
     }
+    
+    [NaughtyAttributes.Button]
+    private void AddCoin()
+    {
+        AddByType(ItemType.COIN);
+    }
+
+    [NaughtyAttributes.Button]
+    private void AddLifePack()
+    {
+        AddByType(ItemType.LIFE_PACK);
+    }
+    
+    [System.Serializable]
+    public class ItenSetup
+    {
+        public ItemType itemType;
+        public SOint soInt;
+    }
+ }
 }
